671ae3fd2a13bf8b11e19cd434f82780d65b5f1d
Pulak5376 <nath15-5376@diu.edu.bd>
2025-04-10 21:18:28 +0600

    problem15

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World App',
+      title: 'Hello_World',
       theme: ThemeData(
         fontFamily: 'RobotoMono', // Custom font
         textTheme: const TextTheme(
